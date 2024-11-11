62d0a03a4b403ff4a98c1db218d8789f4f346496
Laskaridi <anarizhko@gmail.com>
2024-11-11 12:09:24 +0200

    first commit

---
+++ books/src/components/Home/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './components/style/home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Header(props) {
+    return (
+        <div>
+            <FontAwesomeIcon icon="fa-solid fa-book-bookmark" />
+        </div>
+    );
+}
+
+export default Header;
